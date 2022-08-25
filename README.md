7d80c4fa8fa906247910a16cefe9cdafd6b61dc6
kyoukawa <jiangchuan13041043534@hotmail.com>
2022-08-25 16:45:33 +0800

    rebuild

---
+++ README.md
@@ -0,0 +1,53 @@
+# fake-tapechat
+一个在朋友圈吹牛的产物(
+
+~~但是这却是我第一个正经的开源项目~~
+
+# 前期准备
+	作为科技爱好者,并为了展现实力,一定会做全平台的应用
+	我要做的,大概需要html+css+js+python+数据库+(Flutter/swift+swiftUI)+(java/kotlin)+(vercel/electron
+## 网页
+	家境贫寒没钱买云服务器,sb政府和运营商又不给公网ip了,所以就要利用白嫖的力量(
+	web仅有姐姐上大学的html+css+js方面的书里的知识(css完全不会
+### 网页搭建
+	发现了vercel和github page两个可以白嫖的平台
+### 后端
+	会点python,但是完全不会后端
+### 数据库
+	完全不会(
+
+## Android
+	会点kotlin, java也还行,简单页面搞得定(搭配YouTube和BilBili)
+
+## iOS
+	非常熟练,swift非常人性化,代码很简单(
+
+## 电脑端
+	windows开发,也就会个hello world和五子棋
+
+	macOS开发和iOS一起就做了
+
+	Linux不会
+
+	方案:electron(网页套壳)
+
+	还是活成了讨厌的模样
+
+哪位大佬可以提供帮助,请联系我,谢谢.
+
+ps:本项目采用MIT协议开源.
+
+# 0.0.1
+基于Python的flask模块
+
+## 配置
+```shell
+pip install flask
+```
+
+## 运行
+```shell
+python3 main.py
+```
+
+ps:感谢cyh提供的代码
---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def main_page():
+    return render_template("index.html")
+
+@app.route('/signup')
+def new_user():
+    with open('./data/user_data.json','r',encoding='utf8')as fp:
+        user_data = json.load(fp)
+    name = request.values.get("username")
+    password = request.values.get("password")
+    if name not in user_data:
+        user_data[name] = password
+    else:
+        return render_template("error.html")
+    with open('./data/user_data.json','w',encoding='utf8')as fp:
+        json.dump(user_data,fp,ensure_ascii=False)
+    return render_template("signup.html")
+
+@app.route('/ask')
+def ask_question():
+    with open('./data/data.json','r',encoding='utf8')as fp:
+        json_data = json.load(fp)
+        print(json_data)
+        # print(type(json_data))
+    name = request.values.get("username")
+    question = request.values.get("question")
+    if name not in json_data:
+        json_data[name]=[]
+    json_data[name].append(question)
+    with open("./data/data.json",'w',encoding='utf8') as fp:
+        json.dump(json_data,fp,ensure_ascii=False)
+    return render_template("ask.html")
+
+@app.route('/answer')
+def answer_question():
+    with open('./data/data.json','r',encoding='utf8')as fp:
+        json_data = json.load(fp)
+    with open('./data/user_data.json','r',encoding='utf8')as fp:
+        user_data = json.load(fp)
+    name = request.values.get("username")
+    password = request.values.get("password")
+    if name == None:
+        return render_template("answer.html")
+    if name not in user_data:
+        return render_template("error.html")
+    if password == user_data[name]:
+        return render_template("answer.html", questions=json_data[name])
+    else:
+        return render_template("password_error.html")
+
+app.run()
