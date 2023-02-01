d36ee113b66d801a9847562050656674c578d1f3
kyoukawa <jiangchuan13041043534@gmail.com>
2023-02-01 17:06:40 +0800

    much more update

---
+++ static/script.js
@@ -0,0 +1,18 @@
+function signup_button() {
+    window.location.href = "/signup";
+}
+function ask_button() {
+    window.location.href = "/ask";
+}
+function check_button() {
+    window.location.href = "/check";
+}
+function answer_button() {
+    window.location.href = "/answer";
+}
+function admin_button() {
+    window.location.href = "/admin";
+}
+function clear_button() {
+    window.location.href = "/clear";
+} 
